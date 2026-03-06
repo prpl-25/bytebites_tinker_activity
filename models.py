6f1737c9aedfa48e5fc05ce190d33850930e0f18
Sindhu Vadapalli <145177344+prpl-25@users.noreply.github.com>
2026-03-06 12:58:47 -0700

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,6 @@
+#Four classes for this model
+#Customer, Transaction, FoodItem, Menu
+#Customer: has name, transaction history, and a method to get purchase history, add tranmsaction, and get name
+#Food Item: has name, price, category, popularity rating and method to get name and methods to get attributes
+#Menu: has a list of food items and method to get food items, add food item, and remove food item
+#Transaction: has selected items, customer details, methods to add item, compute total, and get selected items
